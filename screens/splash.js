21bbb28ae60f2ea3618d7faeec0905bd6aa43bcf
Nicholas Kolodziej <Nicholas.Kolodziej@ibm.com>
2023-09-20 23:42:34 -0500

    updated splash screen

---
+++ screens/splash.js
@@ -0,0 +1,39 @@
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+function handleNewGame(){
+    let modal = document.getElementById("modal");
+    modal.style.display = "block";
+    setModalContent(newGame);
+}
+
+function handleDeleteGame(){
+    try{
+        if(localStorage.getItem("dovetail")){
+            let modal = document.getElementById("modal");
+            modal.style.display = "block";
+            setModalContent(deleteGame);
+        }else{
+            setSnackbar(`<p>Error finding game to delete.</p>`);
+        }
+    }catch(error){
+        console.log("Error finding game to delete:", error)
+        setSnackbar(`<p>Error finding game to delete.</p>`);
+    }
+}
+
+function splash(){
+    return `
+        <center>
+            <div class="page-container">
+                <h1 class="title">Dovetail</h1>
+                <button onclick="handleNewGame()">New Game</button>
+                <button onclick="loadGame(true);">Load Game</button>
+                <button onclick="handleDeleteGame()">Delete Game</button>
+            </div>
+        </center>
+    `
+}
