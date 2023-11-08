cca4f44761552b15eeeddf72a65307a4f160803d
Nicholas Kolodziej <Nicholas.Kolodziej@ibm.com>
2023-11-08 17:37:25 -0600

    adjusted gamepad

--- screens/game.js
+++ screens/game.js
@@ -44,83 +44,65 @@ function game() {
             <div id="status-update" class="status-update"></div>
 
             <div id='gamepad-container' class='gamepad-container'>
-                <div id='gamepad-directional' class='gamepad-block'>
-                    <table>
-                        <tr>
-                            <td>&nbsp;</td>
-                            <td>
-                                <div id='gamepad-up' class='gamepad-arrow' onclick="keyPress({key: 'arrowup'});">
-                                    <span class="material-symbols-outlined">
-                                        keyboard_arrow_up
-                                    </span>
-                                </div>
-                            </td>
-                            <td>&nbsp;</td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <div id='gamepad-left' class='gamepad-arrow' onclick="keyPress({key: 'arrowleft'});">
-                                    <span class="material-symbols-outlined">
-                                        keyboard_arrow_left
-                                    </span>
-                                </div>
-                            </td>
-                            <td>&nbsp;</td>
-                            <td>
-                                <div id='gamepad-right' class='gamepad-arrow' onclick="keyPress({key: 'arrowright'});">
-                                    <span class="material-symbols-outlined">
-                                        keyboard_arrow_right
-                                    </span>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>&nbsp;</td>
-                            <td>
-                                <div id='gamepad-down' class='gamepad-arrow' onclick="keyPress({key: 'arrowdown'});">
-                                    <span class="material-symbols-outlined">
-                                        keyboard_arrow_down
-                                    </span>
-                                </div>
-                            </td>
-                            <td>&nbsp;</td>
-                        </tr>
-                    </table>
+                <div style="float: left">
+                    <div id='gamepad-directional' class='gamepad-block'>
+                        <table>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td>
+                                    <div id='gamepad-up' class='gamepad-arrow' onclick="keyPress({key: 'arrowup'});">
+                                        <span class="material-symbols-outlined">
+                                            keyboard_arrow_up
+                                        </span>
+                                    </div>
+                                </td>
+                                <td>&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div id='gamepad-left' class='gamepad-arrow' onclick="keyPress({key: 'arrowleft'});">
+                                        <span class="material-symbols-outlined">
+                                            keyboard_arrow_left
+                                        </span>
+                                    </div>
+                                </td>
+                                <td>&nbsp;</td>
+                                <td>
+                                    <div id='gamepad-right' class='gamepad-arrow' onclick="keyPress({key: 'arrowright'});">
+                                        <span class="material-symbols-outlined">
+                                            keyboard_arrow_right
+                                        </span>
+                                    </div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td>
+                                    <div id='gamepad-down' class='gamepad-arrow' onclick="keyPress({key: 'arrowdown'});">
+                                        <span class="material-symbols-outlined">
+                                            keyboard_arrow_down
+                                        </span>
+                                    </div>
+                                </td>
+                                <td>&nbsp;</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
 
                 <div style="float: right">
-                <div id='gamepad-icons' class='gamepad-block'>
-                    <div id='gamepad-map' class='gamepad-arrow' onclick="keyPress({key: 'm'});">
-                        <span class="material-symbols-outlined">
-                            map
-                        </span>
-                    </div>
-
-                    <br />
-                    <div id='gamepad-inventory' class='gamepad-arrow' onclick="keyPress({key: 'i'});">
-                        <span class="material-symbols-outlined">
-                            backpack
-                        </span>
-                    </div>
-
-                    <br />
-                    <div id='gamepad-build' class='gamepad-arrow' onclick="keyPress({key: 'b'});">
-                        <span class="material-symbols-outlined">
-                            construction
-                        </span>
-                    </div>
-                </div>
-
-                <div id='gamepad-action' class='gamepad-block'>
-                    <div id='gamepad-action' class='gamepad-action' onclick="keyPress({key: 'a'});">
-                        <div style="display: inline-block; position: relative; top: 45px; left: 35px;">
-                            <span class="material-symbols-outlined">
-                                target
-                            </span>
+                    <div id='gamepad-action' class='gamepad-block'>
+                        <div id='gamepad-action' class='gamepad-action' onclick="keyPress({key: 'a'});">
+                            <div style="display: inline-block; position: relative; top: 45px; left: 35px;">
+                                <span class="material-symbols-outlined">
+                                    target
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
-                </div>
+
+            </div>
         </div>
         <script>
             ${renderMap(HERO.location)}
